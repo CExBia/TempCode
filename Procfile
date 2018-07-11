26566df65ae090873405b2412cf2d1862f6638ac
CExBia <40669666+CExBia@users.noreply.github.com>
2018-07-11 11:07:57 -0300

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python Request_Openw.py
---
+++ teste12.py
@@ -0,0 +1,12 @@
+#enconding utf-8
+import requests
+import json
+city_name = str(input(''))
+
+r = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+city_name+'&APPID=18db156f5ef1c6bdad1be1c5072fa282')
+r2 = r.json()
+r3 = json.dumps(r2)
+temp = float(r2['main']['temp']) - 273.15
+temp_int = int(temp)
+
+print str(temp_int)+' '+'Graus Celsius'
